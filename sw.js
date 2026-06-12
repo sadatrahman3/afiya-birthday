090cbba21d45a138e41ae8e58e7a1073eb5726bd
sadatrahman3 <sadatrahman3@users.noreply.github.com>
2026-06-13 02:46:38 +0600

    Add blow candles, sparkles, fireworks, share card, multi-language, control panel, smoother transitions, PWA

---
+++ sw.js
@@ -0,0 +1,48 @@
+const CACHE = 'afiya-birthday-v1';
+const URLS = [
+  './',
+  'index.html',
+  'css/style.css',
+  'js/script.js',
+  'manifest.json',
+  'icons/icon-192.svg',
+  'icons/icon-512.svg'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(URLS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  const url = new URL(e.request.url);
+
+  // Cache photos
+  if (url.pathname.startsWith('/photos/')) {
+    e.respondWith(
+      caches.open(CACHE).then(cache =>
+        fetch(e.request).then(res => {
+          cache.put(e.request, res.clone());
+          return res;
+        }).catch(() => caches.match(e.request))
+      )
+    );
+    return;
+  }
+
+  // Cache-first for everything else
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
